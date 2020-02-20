64846a8d235cdd4b501ba02850ab7fbc48993ad4
wonjun <dnjswns2280@naver.com>
2020-02-20 23:17:59 +0900

    데이터 파일 나누기

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//C:\Bitnami\wampstack-7.3.13-0\apache2\nodejun
+
 var http = require('http');
 var fs = require('fs');
 //url이라는 모듈을 사용
@@ -17,27 +19,29 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-      </p>
-    </body>
-    </html>
-    `;
-    response.end(template);
+
+    fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.end(template);
+    })
 
 });
 app.listen(3000);
---
+++ nodejs/fileread.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+
+fs.readFile('sample.txt', 'utf8', function(err,data){
+  console.log(data);
+});
