65f7a751b5ac875b5096877e7a6c975391acc66d
wonjun <dnjswns2280@naver.com>
2020-02-21 17:17:34 +0900

    동적 웹 사이트

--- main.js
+++ main.js
@@ -4,6 +4,34 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function temp(title,list,body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function temp_list(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -14,62 +42,19 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var list = temp_list(filelist);
+          var template = temp(title,list,`<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
         })
 
 
-
       } else {
         fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = temp_list(filelist);
+            var template = temp(title,list,`<h2>${title}</h2><p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -79,8 +64,6 @@ var app = http.createServer(function(request,response){
       response.writeHead(404);
       response.end('Not found');
     }
-
-
 
 });
 app.listen(3000);
---
+++ syntax/func.js
@@ -0,0 +1,20 @@
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
+
+console.log(Math.round(1.6)); //2
+
+function sum(a,b){ //parameter 매개변수
+  return a+b;
+}
+
+console.log(sum(2,5)); //argument 인자
