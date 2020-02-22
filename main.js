4554450a2fa8c9f5a12382265a9666144a3a3c15
wonjun <dnjswns2280@naver.com>
2020-02-22 19:10:40 +0900

    CRUD

--- main.js
+++ main.js
@@ -61,7 +61,14 @@ var app = http.createServer(function(request,response){
             var list = temp_list(filelist);
             var template = temp(title,list,
               `<h2>${title}</h2><p>${description}</p>`,
-              `<a href = "/create">create</a> <a href = "/update?id=${title}">update</a>`);
+              `<a href = "/create">create</a>
+              <a href = "/update?id=${title}">update</a>
+              <!-- post 방식 -->
+              <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`);
+
             response.writeHead(200);
             response.end(template);
           });
@@ -101,7 +108,6 @@ var app = http.createServer(function(request,response){
         });
       });
 
-
     } else if(pathname === '/update'){
       // 글 내용 업데이트
       fs.readdir('./data', function(error, filelist){
@@ -121,6 +127,42 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
+      });
+
+    } else if(pathname === '/update_process'){
+      // 글을 업데이트 해줌
+      var body = '';
+      request.on('data', function(data){
+        body = body+data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`,`data/${title}`,function(error){
+          fs.writeFile(`data/${title}`,description,'utf8',function(err){
+            //302 = redirection
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          });
+        })
+      });
+
+    } else if(pathname === '/delete_process'){
+      // 글을 업데이트 해줌
+      var body = '';
+      request.on('data', function(data){
+        body = body+data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`,function(error){
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        })
+
       });
 
     } else {
---
+++ syntax/object.js
@@ -0,0 +1,1 @@
+var members = ['one','jun','hi'];
